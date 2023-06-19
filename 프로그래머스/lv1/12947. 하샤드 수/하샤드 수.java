ff5596b3da26a55c0da07ac4363da6863272e5d8
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-06-19 14:33:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,19 +1,6 @@
 class Solution {
     public boolean solution(int x) {
-        
-        int copy_x = x;
-        
-        int sum = 0;
-        while(copy_x!=0)
-        {
-            sum += copy_x % 10;
-            copy_x /= 10;
-        }
-        
-        if(x%sum==0){
-            return true;
-        }
-        else
-            return false;
+        boolean answer = true;
+        return answer;
     }
 }
