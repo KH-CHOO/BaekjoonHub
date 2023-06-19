ba7c571008f8c2b0418429ac4a5bb66cdc946d9b
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-06-19 10:05:10 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,9 +4,11 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        while(n>0){
+            answer += n%10;
+            n = n/10;
+        }
+        
         return answer;
     }
 }
