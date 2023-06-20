af619774289becd670c531625973e300d858e5bf
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-06-20 14:16:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.66 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,6 +1,32 @@
+
+
+ import java.util.Arrays;
+ import java.util.Comparator;
+
 class Solution {
+
     public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        return answer;
+        Arrays.sort(strings, new CharComparator(n));
+		return strings;
     }
+}
+
+class CharComparator implements Comparator<String>{
+	private int index;
+	public CharComparator(int index){
+		this.index = index;
+	}
+	
+	@Override
+	public int compare(String s1, String s2){
+		char c1 = s1.charAt(index);
+		char c2 = s2.charAt(index);
+		if (c1 == c2) {
+            return s1.compareTo(s2); 
+			// Sort lexicographically when characters are the same
+        }
+		return Character.compare(c1,c2);
+	}
+
+
 }
